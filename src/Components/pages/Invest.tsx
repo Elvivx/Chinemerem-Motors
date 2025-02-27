24c7f96b5805417fdbb317e76cc57b0d8ab64602
Elvis <Elvisraphael00@gmail.com>
2025-02-27 13:14:43 +0100

    `Refactored Invest page with updated content and images`

--- src/Components/pages/Invest.tsx
+++ src/Components/pages/Invest.tsx
@@ -15,31 +15,34 @@ function Invest() {
         <p>Grow your wealth with strategic investments. Join us in creating a prosperous future with high-yield opportunities tailored for you.</p>
       </div>
       <div className='flex flex-wrap gap-5 w-[1000px] m-auto my-12'>
-        <div className='w-[300px] bg-red-500 p-5 rounded-xl'>
+        <div className='w-[300px] bg-red-500 p-5 rounded-xl flex flex-col'>
           <div className='border-2 border-red-400 w-full h-[200px]'>
-            <img src='../../assets/imgs/jp-valery-lVFoIi3SJq8-unsplash.jpg' alt='#' className='object-cover object-center w-full h-full' />
+            <img src='../../public/imgs/sedan.jpg' alt='#' className='object-cover object-center w-full h-full' />
           </div>
           <h1 className='font-bold text-2xl my-5'>Car Purchase</h1>
           <p> Explore our premium selection of vehicles at unbeatable prices. Drive home your dream car with flexible financing options and hassle-free purchases.</p>
-          <button className='p-3 rounded-lg font-bold text-black bg-white w-full mt-5'>Get Started</button>
+          <button className='p-3 rounded-lg font-bold text-black bg-white w-full mt-auto'>Get Started</button>
         </div>
 
         <div className='w-[300px] bg-red-500 p-5 rounded-xl'>
           <div className='border-2 border-red-400 w-full h-[200px]'>
             <img src='#' alt='#' className='object-cover object-center w-full h-full' />
           </div>
-          <h1 className='font-bold text-2xl my-5'>Car Purchase</h1>
-          <p> Explore our premium selection of vehicles at unbeatable prices. Drive home your dream car with flexible financing options and hassle-free purchases.</p>
+          <h1 className='font-bold text-2xl my-5'>Bus Purchase</h1>
+          <p>
+            Enjoy competitive pricing, flexible financing options, and expert guidance throughout the buying process. Take the next step in your transport journey—browse our collection and make a
+            smart investment today!
+          </p>
           <button className='p-3 rounded-lg font-bold text-black bg-white w-full mt-5'>Get Started</button>
         </div>
 
-        <div className='w-[300px] bg-red-500 p-5 rounded-xl'>
+        <div className='w-[300px] bg-red-500 p-5 rounded-xl flex flex-col'>
           <div className='border-2 border-red-400 w-full h-[200px]'>
-            <img src='#' alt='#' className='object-cover object-center w-full h-full' />
+            <img src='../../../public/imgs/keke.jpg' alt='#' className='object-cover object-center w-full h-full' />
           </div>
-          <h1 className='font-bold text-2xl my-5'>Car Purchase</h1>
-          <p> Explore our premium selection of vehicles at unbeatable prices. Drive home your dream car with flexible financing options and hassle-free purchases.</p>
-          <button className='p-3 rounded-lg font-bold text-black bg-white w-full mt-5'>Get Started</button>
+          <h1 className='font-bold text-2xl my-5'>Tricycle Purchase</h1>
+          <p> Affordable, durable, and fuel-efficient—perfect for personal or business use. Get yours now and start riding towards success!</p>
+          <button className='p-3 rounded-lg font-bold text-black bg-white w-full mt-auto'>Get Started</button>
         </div>
       </div>
       <Footer />
